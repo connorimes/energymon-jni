7c64e2d2308deeb5604d1c0d6429accf4aae6c67
Connor Imes <connor.k.imes@gmail.com>
2015-11-02 13:15:34 -0600

    Manage nativeId to prevent JNI access after finishing

--- energymon/src/main/java/edu/uchicago/cs/energymon/DefaultEnergyMonJNI.java
+++ energymon/src/main/java/edu/uchicago/cs/energymon/DefaultEnergyMonJNI.java
@@ -5,14 +5,20 @@ import java.math.BigInteger;
 /**
  * Gets a default energymon implementation and exposes methods for performing
  * operations on it. This implementation is a simple wrapper around the JNI
- * functions. It is NOT thread safe and should be synchronized externally.
- * Additionally, it does NOT ensure proper protocol (i.e. initialization before
- * access, no access after destruction).
+ * functions.
+ * 
+ * This implementation is <b>NOT</b> thread safe and should be synchronized
+ * externally. Additionally, it does <b>NOT</b> ensure proper protocol (e.g.
+ * initialization before access). Attempting to perform operations after
+ * {@link #finish()} is called will result in an {@link IllegalStateException}.
+ * 
+ * Failure to allocate the native resources also results in an
+ * {@link IllegalStateException} in the constructor.
  * 
  * @author Connor Imes
  */
 public class DefaultEnergyMonJNI implements EnergyMon {
-	private final int nativeId;
+	private volatile int nativeId;
 
 	/**
 	 * Create a {@link DefaultEnergyMonJNI}.
@@ -31,8 +37,13 @@ public class DefaultEnergyMonJNI implements EnergyMon {
 	 * Initialize the energymon implementation (required).
 	 * 
 	 * @return 0 on success, anything else on failure
+	 * @throws IllegalStateException
+	 *             if already finished
 	 */
 	public int init() {
+		if (nativeId < 0) {
+			throw new IllegalStateException("Already finished");
+		}
 		return EnergyMonJNI.energymonInit(nativeId);
 	}
 
@@ -40,8 +51,13 @@ public class DefaultEnergyMonJNI implements EnergyMon {
 	 * Read the energy data in microjoules.
 	 * 
 	 * @return microjoules
+	 * @throws IllegalStateException
+	 *             if already finished
 	 */
 	public BigInteger readTotal() {
+		if (nativeId < 0) {
+			throw new IllegalStateException("Already finished");
+		}
 		return toUnsignedBigInteger(EnergyMonJNI.energymonReadTotal(nativeId));
 	}
 
@@ -51,17 +67,29 @@ public class DefaultEnergyMonJNI implements EnergyMon {
 	 * be used after this function is called!
 	 * 
 	 * @return 0 on success, anything else on failure
+	 * @throws IllegalStateException
+	 *             if already finished
 	 */
 	public int finish() {
-		return EnergyMonJNI.energymonFinish(nativeId);
+		if (nativeId < 0) {
+			throw new IllegalStateException("Already finished");
+		}
+		int result = EnergyMonJNI.energymonFinish(nativeId);
+		nativeId = -1;
+		return result;
 	}
 
 	/**
 	 * Get a human-readable name for the energymon source.
 	 * 
 	 * @return source
+	 * @throws IllegalStateException
+	 *             if already finished
 	 */
 	public String getSource() {
+		if (nativeId < 0) {
+			throw new IllegalStateException("Already finished");
+		}
 		return EnergyMonJNI.energymonGetSource(nativeId);
 	}
 
@@ -69,8 +97,13 @@ public class DefaultEnergyMonJNI implements EnergyMon {
 	 * Get the refresh interval of the energymon in microseconds.
 	 * 
 	 * @return interval
+	 * @throws IllegalStateException
+	 *             if already finished
 	 */
 	public BigInteger getInterval() {
+		if (nativeId < 0) {
+			throw new IllegalStateException("Already finished");
+		}
 		return toUnsignedBigInteger(EnergyMonJNI.energymonGetInterval(nativeId));
 	}
 
